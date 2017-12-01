a7b95136e117f3ef188da424dab0a656abcd9ed2
kaflan <kaf_90@mail.ru>
2017-12-01 17:49:51 +0200

    add :react-ant

--- src/redux/index.js
+++ src/redux/index.js
@@ -1,13 +1,10 @@
 import {createStore, applyMiddleware} from 'redux'
 import reducer from './reducer'
-import logger from 'redux-logger'
-import {routerMiddleware} from 'react-router-redux'
 import createSagaMiddleware from 'redux-saga'
-import history from '../history'
 import rootSaga from './saga'
 
 const sagaMiddleware = createSagaMiddleware()
-const enhancer = applyMiddleware(sagaMiddleware, routerMiddleware(history), logger)
+const enhancer = applyMiddleware(sagaMiddleware)
 
 const store = createStore(reducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),enhancer)
 window.store = store
