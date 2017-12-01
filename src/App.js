30a40d1d56b8c588a244b0c4127c847adc62c6ea
kaflan <kaf_90@mail.ru>
2017-12-02 01:53:15 +0200

    add route

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@ import React, { Component } from 'react';
 import { Provider } from "react-redux";
 import store from './redux';
 import './App.css';
+import createHistory from 'history/createBrowserHistory';
+import {ConnectedRouter} from 'react-router-redux';
 
 class App extends Component {
   render() {
     return (
       <Provider store={store}>
-        <div>Hello</div> 
+       <ConnectedRouter history={createHistory()}>
+        
+       </ConnectedRouter> 
       </Provider>
     );
   }
---
+++ src/components/Root.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import {Route} from 'react-router-dom'
+
+export default class Root extends Component {
+    render () {
+        return(
+            <div>
+                {/* <Route path="/auth" component={AuthPage}/>
+                <Route path="/auth" component={AuthPage}/> */}
+            </div>    
+        )
+    }
+}
